45248107fb9efd6d1b381f20af53831acb6afeb0
Joachim Metz <joachim.metz@gmail.com>
2022-02-15 21:54:12 +0100

    Worked on Windows Defender scan DetectionHistory file

--- dtformats/detection_history.py
+++ dtformats/detection_history.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Windows Defender scan DetectionHistory files."""
 
+from dtfabric.runtime import data_maps as dtfabric_data_maps
+
 from dtformats import data_format
 
 
@@ -11,6 +13,21 @@ class WindowsDefenderScanDetectionHistoryFile(data_format.BinaryDataFile):
   # the dtFabric definition file.
   _FABRIC = data_format.BinaryDataFile.ReadDefinitionFile(
       'detection_history.yaml')
+
+  _DEBUG_INFO_THREAT_TRACKING_HEADER = [
+      ('version', 'Version', '_FormatIntegerAsDecimal'),
+      ('header_size', 'Header size', '_FormatIntegerAsDecimal'),
+      ('values_data_size', 'Values data size', '_FormatIntegerAsDecimal'),
+      ('total_data_size', 'Total data size', '_FormatIntegerAsDecimal'),
+      ('unknown2', 'Unknown2', '_FormatIntegerAsHexadecimal8')]
+
+  _DEBUG_INFO_THREAT_TRACKING_VALUE = [
+      ('key_string_size', 'Key string size', '_FormatIntegerAsDecimal'),
+      ('key_string', 'Key string', '_FormatString'),
+      ('value_type', 'Value type', '_FormatIntegerAsHexadecimal8'),
+      ('value_integer', 'Value integer', '_FormatIntegerAsDecimal'),
+      ('value_string_size', 'Value string size', '_FormatIntegerAsDecimal'),
+      ('value_string', 'Value string', '_FormatString')]
 
   _DEBUG_INFO_VALUE = [
       ('data_size', 'Data size', '_FormatIntegerAsDecimal'),
@@ -31,8 +48,8 @@ class WindowsDefenderScanDetectionHistoryFile(data_format.BinaryDataFile):
       {0: 'UnknownMagic2',
        1: 'Resource type',
        2: 'Resource location',
-       4: 'Threat data size',
-       5: 'Threat data',
+       4: 'Threat tracking data size',
+       5: 'Threat tracking data',
        6: 'Unknown date and time1',
        12: 'User/System account name1',
        18: 'Unknown date and time2',
@@ -87,6 +104,97 @@ class WindowsDefenderScanDetectionHistoryFile(data_format.BinaryDataFile):
       46: 'VULNERABILTIY',
       47: 'POLICY'}
 
+  def _ReadThreatTrackingData(self, threat_tracking_data, file_offset):
+    """Reads the threat tracking data.
+
+    Args:
+      threat_tracking_data (bytes): threat tracking data.
+      file_offset (int): offset of the threat tracking data relative to
+          the start of the file.
+
+    Raises:
+      IOError: if the threat tracking data cannot be read.
+    """
+    if self._debug:
+      self._DebugPrintText((
+          '\nReading threat tracking data at offset: {0:d} '
+          '(0x{0:08x})\n').format(file_offset))
+
+    data_type_map = self._GetDataTypeMap('uint32le')
+
+    values_data_size = self._ReadStructureFromByteStream(
+        threat_tracking_data, 0, data_type_map, 'values data size')
+
+    if values_data_size != 1:
+      values_data_offset = 4
+      values_data_end_offset = values_data_size
+    else:
+      header = self._ReadThreatTrackingHeader(threat_tracking_data)
+
+      values_data_offset = header.header_size + 4
+      values_data_end_offset = header.total_data_size
+
+    while values_data_offset < values_data_end_offset:
+      _, data_size = self._ReadThreatTrackingValue(
+          threat_tracking_data[values_data_offset:],
+          file_offset + values_data_offset)
+      values_data_offset += data_size
+
+  def _ReadThreatTrackingHeader(self, threat_tracking_data):
+    """Reads the threat tracking header.
+
+    Args:
+      threat_tracking_data (bytes): threat tracking data.
+
+    Returns:
+      threat_tracking_header: threat tracking header.
+
+    Raises:
+      IOError: if the threat tracking header cannot be read.
+    """
+    data_type_map = self._GetDataTypeMap('threat_tracking_header')
+
+    threat_tracking_header = self._ReadStructureFromByteStream(
+        threat_tracking_data, 0, data_type_map, 'threat tracking header')
+
+    if self._debug:
+      self._DebugPrintStructureObject(
+          threat_tracking_header, self._DEBUG_INFO_THREAT_TRACKING_HEADER)
+
+    if threat_tracking_header.header_size > 20:
+      # TODO: debug print additional data.
+      pass
+
+    return threat_tracking_header
+
+  def _ReadThreatTrackingValue(self, threat_tracking_data, file_offset):
+    """Reads the threat tracking value.
+
+    Args:
+      threat_tracking_data (bytes): threat tracking data.
+      file_offset (int): offset of the threat tracking data relative to
+          the start of the file.
+
+    Returns:
+      tuple[threat_tracking_value, int]: threat tracking value and data size.
+
+    Raises:
+      IOError: if the threat tracking value cannot be read.
+    """
+    data_type_map = self._GetDataTypeMap('threat_tracking_value')
+
+    context = dtfabric_data_maps.DataTypeMapContext()
+
+    threat_tracking_value = self._ReadStructureFromByteStream(
+        threat_tracking_data, file_offset, data_type_map,
+        'threat tracking value', context=context)
+
+    if self._debug:
+      self._DebugPrintStructureObject(
+          threat_tracking_value, self._DEBUG_INFO_THREAT_TRACKING_VALUE)
+
+    return threat_tracking_value, context.byte_size
+
   def _ReadValue(self, file_object, file_offset):
     """Reads the value.
 
@@ -132,34 +240,39 @@ class WindowsDefenderScanDetectionHistoryFile(data_format.BinaryDataFile):
     Raises:
       ParseError: if the file cannot be read.
     """
-    value_objects = []
+    value_tuples = []
 
     file_offset = 0
     while file_offset < self._file_size:
       value_object = self._ReadValue(file_object, file_offset)
-      value_objects.append(value_object)
+      value_tuples.append((file_offset, value_object))
 
       file_offset = file_object.tell()
 
     if self._debug:
       value_index_set = 0
       value_index = 0
-      for value_object in value_objects:
+      for file_offset, value_object in value_tuples:
         if value_object == 'Magic.Version:1.2':
           if value_index_set < 2:
             value_index_set += 1
           value_index = 0
 
-        description = self._VALUE_DESCRIPTIONS[value_index_set].get(
-            value_index, 'UNKNOWN_{0:d}_{1:d}'.format(
-                value_index_set, value_index))
+        if (value_index_set, value_index) == (2, 5):
+          self._ReadThreatTrackingData(value_object, file_offset + 8)
 
-        if (value_index_set, value_index) == (1, 4):
-          value_string = '{0!s} ({1:s})'.format(
-              value_object, self._CATEGORY_NAME.get(value_object, 'UNKNOWN'))
         else:
-          value_string = '{0!s}'.format(value_object)
+          description = self._VALUE_DESCRIPTIONS[value_index_set].get(
+              value_index, 'UNKNOWN_{0:d}_{1:d}'.format(
+                  value_index_set, value_index))
 
-        self._DebugPrintValue(description, value_string)
+          if (value_index_set, value_index) == (1, 4):
+            value_string = '{0!s} ({1:s})'.format(
+                value_object, self._CATEGORY_NAME.get(value_object, 'UNKNOWN'))
+
+          else:
+            value_string = '{0!s}'.format(value_object)
+
+          self._DebugPrintValue(description, value_string)
 
         value_index += 1
