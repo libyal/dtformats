2f6c5ccde4b38a303c30df9b6ff0f9dbce3f411b
Rick Asselin <rickslin@protonmail.com>
2022-08-03 11:37:00 -0400

    implemented a parser for timesync files

--- tests/unified_logging.py
+++ tests/unified_logging.py
@@ -38,6 +38,54 @@ class DSCFileTest(test_lib.BaseTestCase):
     test_file.Open(test_file_path)
 
 
+class TimesyncFileTest(test_lib.BaseTestCase):
+  """Tests for the timesync files."""
+
+  # pylint: disable=protected-access
+
+  def testReadFileRecord(self):
+    """Tests the _ReadRecord method."""
+    output_writer = test_lib.TestOutputWriter()
+    test_file = unified_logging.TimesyncFile(output_writer=output_writer)
+
+    test_file_path = self._GetTestFilePath([
+        'timesync', '0000000000000002.timesync'])
+    self._SkipIfPathNotExists(test_file_path)
+
+    with open(test_file_path, 'rb') as file_object:
+      # Boot record
+      record, _ = test_file._ReadRecord(file_object, 0)
+
+      self.assertEqual(record.signature, b'\xb0\xbb')
+      self.assertEqual(record.size, 48)
+      self.assertEqual(record.timebase_numerator, 125)
+      self.assertEqual(record.timebase_denominator, 3)
+      self.assertEqual(record.timestamp, 1541730321839294000)
+      self.assertEqual(record.time_zone_offset, 480)
+      self.assertEqual(record.daylight_saving_flag, 0)
+
+      # sync record
+      record, _ = test_file._ReadRecord(file_object, 48)
+
+      self.assertEqual(record.signature, b'\x54\x73\x20\x00')
+      self.assertEqual(record.kernel_time, 494027973)
+      self.assertEqual(record.timestamp, 1541730337313716000)
+      self.assertEqual(record.time_zone_offset, 480)
+      self.assertEqual(record.daylight_saving_flag, 0)
+
+  def testReadFileObject(self):
+    """Tests the ReadFileObject method."""
+    output_writer = test_lib.TestOutputWriter()
+    test_file = unified_logging.TimesyncFile(
+        debug=True, output_writer=output_writer)
+
+    test_file_path = self._GetTestFilePath([
+        'timesync', '0000000000000002.timesync'])
+    self._SkipIfPathNotExists(test_file_path)
+
+    test_file.Open(test_file_path)
+
+
 class TraceV3FileTest(test_lib.BaseTestCase):
   """Apple Unified Logging and Activity Tracing (tracev3) file tests."""
 
