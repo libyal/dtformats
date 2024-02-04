6b4f1b446355f6e03470c7021601e0d6f15179d6
Joachim Metz <joachim.metz@gmail.com>
2024-02-04 12:45:12 +0100

    Worked on parser for Mac OS backgrounditems.btm bookmark data

--- dtformats/bookmark_data.py
+++ dtformats/bookmark_data.py
@@ -14,6 +14,7 @@ class MacOSBackgroundItemBookmarkData(data_format.BinaryDataFile):
 
   _DEBUG_INFORMATION = data_format.BinaryDataFile.ReadDebugInformationFile(
       'bookmark_data.debug.yaml', custom_format_callbacks={
+          'array_of_decimals': '_FormatArrayOfIntegersAsDecimals',
           'array_of_tagged_values': '_FormatArrayOfTaggedValues',
           'signature': '_FormatStreamAsString'})
 
@@ -92,6 +93,52 @@ class MacOSBackgroundItemBookmarkData(data_format.BinaryDataFile):
 
     return header
 
+  def _ReadIntegersArray(
+      self, file_object, data_area_offset, data_record_offsets):
+    """Reads an integers array.
+
+    Args:
+      file_object (file): file-like object.
+      data_area_offset (int): offset of the data area relative to the start of
+          the file.
+      data_record_offsets (list[int]): offsets of the entry data records.
+
+    Returns:
+      list[str]: integers.
+    """
+    integers_array = []
+
+    for data_record_offset in data_record_offsets:
+      data_record = self._ReadDataRecord(
+          file_object, data_area_offset + data_record_offset)
+
+      integers_array.append(data_record.integer)
+
+    return integers_array
+
+  def _ReadStringsArray(
+      self, file_object, data_area_offset, data_record_offsets):
+    """Reads a strings array.
+
+    Args:
+      file_object (file): file-like object.
+      data_area_offset (int): offset of the data area relative to the start of
+          the file.
+      data_record_offsets (list[int]): offsets of the entry data records.
+
+    Returns:
+      list[str]: strings.
+    """
+    strings_array = []
+
+    for data_record_offset in data_record_offsets:
+      data_record = self._ReadDataRecord(
+          file_object, data_area_offset + data_record_offset)
+
+      strings_array.append(data_record.string)
+
+    return strings_array
+
   def _ReadTableOfContents(self, file_object, file_offset):
     """Reads a table of contents (TOC).
 
@@ -140,7 +187,7 @@ class MacOSBackgroundItemBookmarkData(data_format.BinaryDataFile):
     data_type_map = self._GetDataTypeMap('uint32le')
 
     data_area_size, _ = self._ReadStructureFromFileObject(
-        file_object, 48, data_type_map, 'data area size')
+        file_object, header.data_area_offset, data_type_map, 'data area size')
 
     if self._debug:
       value_string, _ = self._FormatIntegerAsDecimal(data_area_size)
@@ -149,9 +196,57 @@ class MacOSBackgroundItemBookmarkData(data_format.BinaryDataFile):
       self._DebugPrintText('\n')
 
     table_of_contents = self._ReadTableOfContents(
-        file_object, 48 + data_area_size)
+        file_object, header.data_area_offset + data_area_size)
 
     for tagged_value in table_of_contents.tagged_values:
+      if self._debug:
+        value_string, _ = self._FormatIntegerAsHexadecimal4(
+            tagged_value.value_tag)
+        self._DebugPrintValue('Value tag', value_string)
+
       data_record_offset = (
           header.data_area_offset + tagged_value.value_data_record_offset)
-      self._ReadDataRecord(file_object, data_record_offset)
+      data_record = self._ReadDataRecord(file_object, data_record_offset)
+
+      if data_record.data_type == 0x00000400:
+        # TODO: change dtFabric to support setting float64be directly in struct
+        data_type_map = self._GetDataTypeMap('float64be')
+        cocoa_timestamp = self._ReadStructureFromByteStream(
+            data_record.data, 0, data_type_map, 'Floating-point 64-bit')
+
+        if self._debug:
+          value_string, _ = self._FormatFloatingPoint(cocoa_timestamp)
+          self._DebugPrintValue('Cocoa timestamp', value_string)
+
+          self._DebugPrintText('\n')
+
+      elif tagged_value.value_tag == 0x1004:
+        strings_array = self._ReadStringsArray(
+            file_object, header.data_area_offset, data_record.integers)
+
+        if self._debug:
+          value_string = '/'.join(strings_array)
+          self._DebugPrintValue('Path', value_string)
+
+          self._DebugPrintText('\n')
+
+      elif tagged_value.value_tag == 0x1005:
+        integers_array = self._ReadIntegersArray(
+            file_object, header.data_area_offset, data_record.integers)
+
+        if self._debug:
+          value_string, _ = self._FormatArrayOfIntegersAsDecimals(
+              integers_array)
+          self._DebugPrintValue('Integers array', value_string)
+
+          self._DebugPrintText('\n')
+
+      elif tagged_value.value_tag in (0x1010, 0x2020):
+        data_type_map = self._GetDataTypeMap('bookmark_data_property_flags')
+        property_flags = self._ReadStructureFromByteStream(
+            data_record.data, 0, data_type_map, 'Property flags')
+
+        if self._debug:
+          debug_info = self._DEBUG_INFORMATION.get(
+              'bookmark_data_property_flags', None)
+          self._DebugPrintStructureObject(property_flags, debug_info)
